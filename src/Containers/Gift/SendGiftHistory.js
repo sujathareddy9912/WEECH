1074ee822db67c81677a544998c7acb2dd7019bf
hkrvikrant <vikranttomartech@gmail.com>
2024-02-24 18:21:27 +0530

    Added a base screen for Send Gift History

---
+++ src/Containers/Gift/SendGiftHistory.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import { styles } from './styles';
+
+const SendGiftHistory = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Send History</Text>
+    </View>
+  );
+};
+
+export default SendGiftHistory;

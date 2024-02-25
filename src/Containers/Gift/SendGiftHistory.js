5b79ef3aea0244922be156e0e22aa8b1162b42eb
hkrvikrant <vikranttomartech@gmail.com>
2024-02-25 22:50:53 +0530

    Added send gift history component

--- src/Containers/Gift/SendGiftHistory.js
+++ src/Containers/Gift/SendGiftHistory.js
@@ -1,13 +1,74 @@
-import React from 'react';
-import {View, Text} from 'react-native';
-import { styles } from './styles';
+import React, {useCallback, useState} from 'react';
+import {View, FlatList, Text} from 'react-native';
+import {useFocusEffect} from '@react-navigation/native';
+import {useDispatch, useSelector} from 'react-redux';
 
-const SendGiftHistory = () => {
+import {styles} from './styles';
+import {getSendGiftListAction} from '../../Redux/Action';
+import {MyImage} from '../../Component/commomComponent';
+import {IMAGE_URL} from '../../Services/Api/Common';
+
+export default function SendGiftHistory() {
+  const dispatch = useDispatch();
+  const [sendGiftList, setSendGiftList] = useState([]);
+
+  const state = useSelector(state => {
+    return state;
+  });
+  const {userLoginList} = state.authReducer;
+
+  useFocusEffect(
+    useCallback(() => {
+      dispatch(
+        getSendGiftListAction(
+          {
+            start: 0,
+            limit: 50,
+            userId: userLoginList?.user?._id,
+            search: '',
+          },
+          list => {
+            setSendGiftList(list);
+          },
+        ),
+      );
+    }, []),
+  );
+
+  const GiftHistoryRender = ({item}) => {
+    const {giftDetails, receiverUser, price} = item;
+    return (
+      <View style={styles.renderingContainer}>
+        <MyImage
+          fast
+          source={{uri: `${IMAGE_URL}${giftDetails?.icon}`}}
+          style={styles.renderingIconStyle}
+        />
+        <View style={styles.renderingDetailsContainer}>
+          <Text style={styles.renderingTextStyle}>
+            Gift Name: {giftDetails?.name}
+          </Text>
+          <Text style={styles.renderingTextStyle}>Sended By: {receiverUser[0]?.name}</Text>
+          <Text style={styles.renderingTextStyle}>
+            Quantity: {giftDetails?.calcType} | {price}
+          </Text>
+        </View>
+      </View>
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Send History</Text>
+    <View style={styles.listRenderContainer}>
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        data={sendGiftList}
+        renderItem={GiftHistoryRender}
+        keyExtractor={item => item._id}
+        contentContainerStyle={styles.list}
+        style={{
+          flex: 1,
+        }}
+      />
     </View>
   );
-};
-
-export default SendGiftHistory;
+}

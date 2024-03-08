93a4b6f13f732edf3473ac6052291d72e8d6e6d6
hkrvikrant <vikranttomartech@gmail.com>
2024-03-08 11:10:41 +0530

    commet unused font

--- src/Utils/fontFamily.ts
+++ src/Utils/fontFamily.ts
@@ -4,11 +4,11 @@ import {getFontSize} from './responsive';
 export enum FONT_FAMILY {
   AVENIER_BLACK = 'AvenirLTStd-Black',
   AVENIER_BOOK = 'AvenirLTStd-Book',
-  AVENIER_ROMAN = 'AvenirLTStd-Roman',
+  // AVENIER_ROMAN = 'AvenirLTStd-Roman',
 
   GILROY_BOLD = 'Gilroy-Bold',
-  GILROY_HEAVY = 'Gilroy-Heavy',
-  GILROY_LIGHT = 'Gilroy-Light',
+  // GILROY_HEAVY = 'Gilroy-Heavy',
+  // GILROY_LIGHT = 'Gilroy-Light',
   GILROY_MEDIUM = 'Gilroy-Medium',
   GILROY_REGULAR = 'Gilroy-Regular',
   GILROY_SEMIBOLD = 'Gilroy-Semibold',
@@ -17,34 +17,38 @@ export enum FONT_FAMILY {
 
   MULISH_REGULAR = 'Mulish-Regular',
 
-  POPPINS_BLACK = 'Poppins-Black',
+  // POPPINS_BLACK = 'Poppins-Black',
   POPPINS_BOLD = 'Poppins-Bold',
-  POPPINS_EXTRABOLD = 'Poppins-ExtraBold',
-  POPPINS_ECTRA_LIGHT = 'Poppins-ExtraLight',
-  POPPINS_LIGHT = 'Poppins-Light',
+  // POPPINS_EXTRABOLD = 'Poppins-ExtraBold',
+  // POPPINS_ECTRA_LIGHT = 'Poppins-ExtraLight',
+  // POPPINS_LIGHT = 'Poppins-Light',
   POPPINS_MEDIUM = 'Poppins-Medium',
   POPPINS_REGULAR = 'Poppins-Regular',
   POPPINS_SEMIBOLD = 'Poppins-SemiBold',
-  POPPINS_THIN = 'Poppins-Thin',
+  // POPPINS_THIN = 'Poppins-Thin',
 
   ROBOTO_BLACK = 'Roboto-Black',
-  ROBOTO_BLACK_ITALIC = 'Roboto-BlackItalic',
+  // ROBOTO_BLACK_ITALIC = 'Roboto-BlackItalic',
   ROBOTO_BOLD = 'Roboto-Bold',
-  ROBOTO_BOLD_ITALIC = 'Roboto-BoldItalic',
-  ROBOTO_ITALIC = 'Roboto-Italic',
-  ROBOTO_LIGHT = 'Roboto-Light',
-  ROBOTO_LIGHT_ITALIC = 'Roboto-LightItalic',
+  // ROBOTO_BOLD_ITALIC = 'Roboto-BoldItalic',
+  // ROBOTO_ITALIC = 'Roboto-Italic',
+  // ROBOTO_LIGHT = 'Roboto-Light',
+  // ROBOTO_LIGHT_ITALIC = 'Roboto-LightItalic',
   ROBOTO_MEDIUM = 'Roboto-Medium',
-  ROBOTO_MEDIUM_ITALIC = 'Roboto-MediumItalic',
+  // ROBOTO_MEDIUM_ITALIC = 'Roboto-MediumItalic',
   ROBOTO_REGULAR = 'Roboto-Regular',
-  ROBOTO_THIN = 'Roboto-Thin',
-  ROBOTO_THIN_ITALIC = 'Roboto-ThinItalic',
+  // ROBOTO_THIN = 'Roboto-Thin',
+  // ROBOTO_THIN_ITALIC = 'Roboto-ThinItalic',
 
   SF_PRO_REGULAR = 'SFProDisplay-Regular',
   SF_PRO_BOLD = 'SFProDisplay-Bold',
   SF_PRO_MEDIUM = 'SFProDisplay-Medium',
   SF_PRO_SEMIBOLD = 'SFProDisplay-Semibold',
   SF_PRO_Light = 'SFProDisplay-Light',
+  SFProText_Medium = 'SFProText-Medium',
+  
+  Inter_Medium = 'Inter-Medium'
+
 }
 
 export enum FONT_SIZE {
